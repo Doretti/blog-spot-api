5e6f17907d38a70c2d47e35c537ff8bd3c886186
MaximShemiakin <doretti.female@gmail.com>
2021-10-07 11:19:24 -0700

    connect

--- sequelize.js
+++ sequelize.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
-const sequelize = new Sequelize(process.env.PG_URI)
+const sequelize = new Sequelize(process.env.PG_URI, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+})
 
 module.exports = sequelize
