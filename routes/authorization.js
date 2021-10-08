dd16cfe4a5efbaf4cfb0e35f276eca686d7deb97
MaximShemiakin <doretti.female@gmail.com>
2021-10-08 05:09:52 -0700

    test

--- routes/authorization.js
+++ routes/authorization.js
@@ -6,41 +6,50 @@ const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 router.post('/auth', async (req, res) => {
-  const {
-    username,
-    email,
-    password
-  } = req.body
-
-  const user = (await User.findAll({
-    where: {
+  try {
+    const {
       username,
-      email
-    },
-    raw: true
-  }))
+      email,
+      password
+    } = req.body
 
-  if (user.length) {
+    if (!username || !email || !password) {
+      res.json({
+        mesage: 'Not enough data'
+      }).status(400)
+    }
+  
+    const user = (await User.findAll({
+      where: {
+        username,
+        email
+      },
+      raw: true
+    }))
+  
+    if (!user.length) {
+      res.json({mesage: 'User not found'}).status(400)
+    }
+  
+    const pwdEq = bcrypt.compareSync(password, user[0].password)
+  
+    if (!pwdEq) {
+      return res.json({
+        data: 'Password not correct'
+      }).status(400)
+    }
+  
+    payload = { "userId": user[0].id }
+  
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { algorithm: 'HS256', expiresIn: 60 * 60 })
+  
     res.json({
-      mesage: 'User not found'
-    }).status(400)
+      token
+    })
+  } catch (error) {
+    console.log(error);
+    res.json(error).status(400)
   }
-
-  const pwdEq = bcrypt.compareSync(password, user[0].password)
-
-  if (!pwdEq) {
-    return res.json({
-      data: 'Password not correct'
-    }).status(400)
-  }
-
-  payload = { "userId": user[0].id }
-
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { algorithm: 'HS256', expiresIn: 60 * 60 })
-
-  res.json({
-    token
-  })
 })
 
 module.exports = router
