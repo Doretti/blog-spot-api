51638608273f0f2229ceee8b7db207e5f75f5fca
MaximShemiakin <doretti.female@gmail.com>
2021-10-08 04:36:51 -0700

    test

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(fileUpload({
   createParentPath: true
 }));
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use('/static', express.static(__dirname + '/public'))
 app.use(express.json())
